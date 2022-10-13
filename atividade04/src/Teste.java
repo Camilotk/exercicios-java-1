5759564481be56df077ff1540457f744eed5c432
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-10-13 10:54:06 -0300

    Atividade 4

---
+++ atividade04/src/Teste.java
@@ -0,0 +1,150 @@
+package br.edu.utfpr;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Teste {
+    public static void imprimeVeiculos(Veiculo[] veiculos)  {
+        Arrays.stream(veiculos)
+                .filter(Objects::nonNull)
+                .forEach(veiculo -> {
+                    veiculo.imprimeDados();
+                    System.out.println();
+                });
+    }
+
+    public static Veiculo[] buscaVeiculo(Veiculo[] veiculos, String placa) {
+        return Arrays.stream(veiculos)
+                .filter(veiculo -> veiculo != null)
+                .filter(veiculo -> veiculo.getPlaca().equals(placa))
+                .toArray(Veiculo[]::new);
+    }
+
+    public static void main(String[] args) {
+        var veiculosPasseio = new Passeio[5];
+        var veiculosCarga = new Carga[5];
+        int[] counter = { 0, 0 };
+        int opcao = 0;
+
+        try {
+            while (opcao != 7) {
+                opcao = Leitura.entradaDados(Leitura.Tipo.MENU);
+
+                switch (opcao) {
+                    case 1:
+                        if(counter[0] < veiculosPasseio.length) {
+                            Passeio carro = new Passeio();
+                            System.out.println("Cadastro de Veículo de Passeio");
+                            System.out.println();
+                            System.out.print("Entre a placa: ");
+                            carro.setPlaca(Leitura.entradaDados(Leitura.Tipo.STRING));
+                            Veiculo[] busca = buscaVeiculo(veiculosPasseio, carro.getPlaca());
+                            if (busca.length > 0) {
+                                System.out.println("Veiculo com placa " + carro.getPlaca() + " já cadastrado!");
+                                break;
+                            }
+                            System.out.print("Entre a marca: ");
+                            carro.setMarca(Leitura.entradaDados(Leitura.Tipo.STRING));
+                            System.out.print("Entre a modelo: ");
+                            carro.setModelo(Leitura.entradaDados(Leitura.Tipo.STRING));
+                            System.out.print("Entre a cor: ");
+                            carro.setCor(Leitura.entradaDados(Leitura.Tipo.STRING));
+                            System.out.print("Entre a velocidade máxima: ");
+                            carro.setVelocidadeMaxima(Leitura.entradaDados(Leitura.Tipo.FLOAT));
+                            System.out.print("Entre a quantidade de rodas: ");
+                            carro.setQuantidadeRodas(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                            System.out.print("Entre a quantidade de passageiros: ");
+                            carro.setQuantidadePassageiros(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                            System.out.print("Entre a potência do motor: ");
+                            carro.getMotor().setPotencia(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                            System.out.print("Entre a quantidade de pistões do motor: ");
+                            carro.getMotor().setQuantidadePistoes(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                            veiculosPasseio[counter[0]] = carro;
+                            counter[0]++;
+                            System.out.println("Carro " + carro.getPlaca() + " cadastrado!");
+                        } else {
+                            System.out.println("Limite de veículos cadastrados já atingido!");
+                        }
+                        break;
+                    case 2:
+                        String repetir = "sim";
+                        while(repetir.equals("sim")) {
+                            if (counter[1] < veiculosPasseio.length) {
+                                Carga carga = new Carga();
+                                System.out.println("Cadastro de Veículo de Carga");
+                                System.out.println();
+                                System.out.print("Entre a placa: ");
+                                carga.setPlaca(Leitura.entradaDados(Leitura.Tipo.STRING));
+                                Veiculo[] busca = buscaVeiculo(veiculosCarga, carga.getPlaca());
+                                if (busca.length > 0) {
+                                    System.out.println("Veiculo com placa " + carga.getPlaca() + " já cadastrado!");
+                                    break;
+                                }
+                                System.out.print("Entre a marca: ");
+                                carga.setMarca(Leitura.entradaDados(Leitura.Tipo.STRING));
+                                System.out.print("Entre a modelo: ");
+                                carga.setModelo(Leitura.entradaDados(Leitura.Tipo.STRING));
+                                System.out.print("Entre a cor: ");
+                                carga.setCor(Leitura.entradaDados(Leitura.Tipo.STRING));
+                                System.out.print("Entre a velocidade máxima: ");
+                                carga.setVelocidadeMaxima(Leitura.entradaDados(Leitura.Tipo.FLOAT));
+                                System.out.print("Entre a quantidade de rodas: ");
+                                carga.setQuantidadeRodas(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                                System.out.print("Entre a carga máxima: ");
+                                carga.setCargaMaxima(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                                System.out.print("Entre a tara: ");
+                                carga.setTara(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                                System.out.print("Entre a potência do motor: ");
+                                carga.getMotor().setPotencia(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                                System.out.print("Entre a quantidade de pistões do motor: ");
+                                carga.getMotor().setQuantidadePistoes(Leitura.entradaDados(Leitura.Tipo.INTEGER));
+                                veiculosCarga[counter[1]] = carga;
+                                counter[1]++;
+                                System.out.println("Carro " + carga.getPlaca() + " cadastrado!");
+                                System.out.println("Deseja cadastrar outro veículo? Sim/Não");
+                                repetir = Leitura.entradaDados(Leitura.Tipo.STRING);
+                                repetir = repetir.toLowerCase();
+                            } else {
+                                System.out.println("Limite de veículos cadastrados já atingido!");
+                            }
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Veículos de Passeio Cadastrados: ");
+                        System.out.println();
+                        imprimeVeiculos(veiculosPasseio);
+                        break;
+                    case 4:
+                        System.out.println("Veiculos de Carga Cadastrados");
+                        System.out.println();
+                        imprimeVeiculos(veiculosCarga);
+                        break;
+                    case 5:
+                        System.out.print("Digite a placa do veículo de passeio que deseja buscar: ");
+                        String buscaPasseio = Leitura.entradaDados(Leitura.Tipo.STRING);
+                        Veiculo[] resultadoBuscaPasseio = buscaVeiculo(veiculosPasseio, buscaPasseio);
+                        if(resultadoBuscaPasseio.length > 0) {
+                            resultadoBuscaPasseio[0].imprimeDados();
+                        } else {
+                            System.out.println("Veiculo não encontrado");
+                        }
+                        break;
+                    case 6:
+                        System.out.print("Digite a placa do veículo de carga que deseja buscar: ");
+                        String buscaCarga = Leitura.entradaDados(Leitura.Tipo.STRING);
+                        Veiculo[] resultadoBuscaCarga = buscaVeiculo(veiculosCarga, buscaCarga);
+                        if(resultadoBuscaCarga.length > 0) {
+                            resultadoBuscaCarga[0].imprimeDados();
+                        } else {
+                            System.out.println("Veiculo não encontrado");
+                        }
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro durante a execução do programa!");
+            e.printStackTrace();
+        }
+
+    }
+}
